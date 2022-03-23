270f7afb528b6ecf6923f27a90c8c1a2118d2dd3
Nika <nikolozamgalo@gmail.com>
2022-03-23 15:31:08 +0400

    Hotfix: prod crash

--- front/src/index.js
+++ front/src/index.js
@@ -42,7 +42,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
