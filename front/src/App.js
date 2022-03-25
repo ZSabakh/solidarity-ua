58fa3cf2c95f33c2148d189cdf54384e4b8b38ea
ZSabakh <zsabakhtarishvili@atu.edu>
2022-03-24 20:28:25 -0500

    Added guarded routes, minor responsiveness fix

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import axios from "axios";
 import Home from "./containers/home/Home";
 import About from "./containers/about/About";
@@ -11,11 +11,11 @@ import CreatePost from "./containers/post/CreatePost";
 import ViewPost from "./containers/post/ViewPost";
 import Profile from "./containers/profile/Profile";
 import Footer from "./components/footer/Footer";
-import { InfoProvider } from "./utility/InfoContext";
 import { Wrapper } from "@googlemaps/react-wrapper";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
-import { useEffect } from "react";
+import { InfoContext } from "./utility/InfoContext";
+import { useEffect, useContext } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 if (window.location.hostname === "localhost") {
   axios.defaults.baseURL = process.env.REACT_APP_BASE_URL + "/api";
@@ -26,31 +26,37 @@ if (window.location.hostname === "localhost") {
 axios.defaults.headers.common["Authorization"] = localStorage.getItem("token");
 
 function App() {
+  window.localStorage.setItem("user_culture", "en");
+  const { authorized } = useContext(InfoContext);
+
   useEffect(() => {
     TimeAgo.addDefaultLocale(en);
   }, []);
 
   return (
     <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_CAPTCHA_KEY}>
-      <InfoProvider>
-        <Wrapper apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/post/view/:id" element={<ViewPost />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/register/activate" element={<Activate />} />
-              <Route path="/password/forgot" element={<Forgot />} />
-              <Route path="/password/reset/:token" element={<Reset />} />
-              <Route path="/post/create" element={<CreatePost />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          </Router>
-          <Footer />
-        </Wrapper>
-      </InfoProvider>
+      <Wrapper apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post/view/:id" element={<ViewPost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/register/activate" element={<Activate />} />
+            <Route path="/password/forgot" element={<Forgot />} />
+            <Route path="/password/reset/:token" element={<Reset />} />
+            {authorized ? (
+              <>
+                <Route path="/post/create" element={<CreatePost />} />
+                <Route path="/profile" element={<Profile />} />
+              </>
+            ) : null}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </Wrapper>
     </GoogleReCaptchaProvider>
   );
 }
